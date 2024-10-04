f13c8b08d0fa82c11b831b0280bb0b8afa91f9ef
burnjoe <sabanajholo@gmail.com>
2024-10-04 22:08:08 +0800

    added button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button({}) {
+  return (
+    <button className="h-[45px] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer">
+      Add to list
+    </button>
+  );
+}
