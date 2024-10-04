6ef933296bbab653fe8f5c5a5f7392e86a669ec2
burnjoe <sabanajholo@gmail.com>
2024-10-04 22:07:44 +0800

    added header components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
+import Logo from "./Logo";
+import Counter from "./Counter";
+
 export default function Header() {
   return (
     <header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[0.08]">
-      <img src="https://bytegrad.com/course-assets/react-nextjs/dots.png" />
+      <Logo />
 
-      <p>
-        <b>0</b> / 0 completed
-      </p>
+      <Counter />
     </header>
   );
 }
