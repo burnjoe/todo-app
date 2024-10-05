b6e9064fd585a212f1c8e65a5a699401b085c6f7
burnjoe <sabanajholo@gmail.com>
2024-10-05 16:28:33 +0800

    added styling in TodoList component + added linethrough in list item

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,39 @@
+import DeleteButton from "./DeleteButton";
+
+const initialTodos = [
+  {
+    id: 1,
+    text: "finish springboot project",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    text: "study react with typescript",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    text: "draft a plan for the startup business",
+    isCompleted: false,
+  },
+];
+
 export default function TodoList() {
-  return <ul></ul>;
+  return (
+    <ul>
+      {initialTodos.map((todo) => (
+        <li
+          key={todo.id}
+          className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]"
+        >
+          <span
+            className={`${todo.isCompleted ? "line-through text-[#ccc]" : ""}`}
+          >
+            {todo.text}
+          </span>
+          <DeleteButton />
+        </li>
+      ))}
+    </ul>
+  );
 }
