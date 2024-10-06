f91e8d7a700b265c593c820e1c87217ed1687697
burnjoe <sabanajholo@gmail.com>
2024-10-06 14:19:44 +0800

    added toggling of todo to mark it complete

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -2,7 +2,10 @@
 export default function DeleteButton({ id, setTodos }) {
   return (
     <button
-      onClick={() => {
+      onClick={(e) => {
+        // Prevents the click event from bubbling up to parent elements
+        e.stopPropagation();
+
         // The 'prev' refers to previous state of todos array (current list of todos)
         setTodos((prev) => prev.filter((todo) => todo.id !== id));
       }}
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -26,6 +26,16 @@ export default function TodoList() {
         <li
           key={todo.id}
           className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]"
+          onClick={() => {
+            setTodos(
+              todos.map((t) => {
+                if (t.id === todo.id) {
+                  return { ...t, isCompleted: !t.isCompleted };
+                }
+                return t;
+              })
+            );
+          }}
         >
           <span
             className={`${todo.isCompleted ? "line-through text-[#ccc]" : ""}`}
