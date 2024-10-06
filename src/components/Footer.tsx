8d12306cc55630a429d03ba95cbc35679850d4a1
burnjoe <sabanajholo@gmail.com>
2024-10-06 17:09:41 +0800

    modified footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex justify-between w-[972px] items-center text-[11px] opacity-30 mt-[12px]">
       <small className="text-[11px]">
-        &copy; 2024. Copyright by <b>BNJE</b>.
+        &copy; 2024. Copyright by <b>burnjoe</b>.
       </small>
       <p>
         Version <b>0.0.1</b>
