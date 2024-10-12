5b719356a714de3a0a88a57044f25b5678564e57
burnjoe <sabanajholo@gmail.com>
2024-10-12 21:22:08 +0800

    created lib folder and storing types - better folder structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,14 +4,7 @@ import TodoList from "./TodoList";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 import { useState } from "react";
-
-// Defined the Todo type similar to how we create a record/class in Java
-// Export to enable reusability of this Todo type
-export type Todo = {
-  id: number;
-  text: string;
-  isCompleted: boolean;
-};
+import { Todo } from "../lib/types";
 
 function App() {
   // State
---
+++ src/lib/types.ts
@@ -0,0 +1,7 @@
+// Defined the Todo type similar to how we create a record/class in Java
+// Export to enable reusability of this Todo type
+export type Todo = {
+  id: number;
+  text: string;
+  isCompleted: boolean;
+};
